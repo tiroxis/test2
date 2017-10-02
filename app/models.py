2decb65a9f932261f361263140d2793da32d6af5
tiroxis <tiroxis@gmail.com>
2017-10-02 07:47:58 +0300

    forgot about auth

--- app/models.py
+++ app/models.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from rest_framework.authtoken.models import Token
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.conf import settings
 
+
+# putt to common module
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
 
 class Stat(models.Model):
     country = models.CharField(max_length=50)
--- settings/settings.py
+++ settings/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
 
     'app',
     'shop'
@@ -124,3 +125,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
+    )
+}
